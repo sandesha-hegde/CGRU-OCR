fc0af2b2df65c3a3eee09fa7c86c2de5e5191747
Sandesha Hegde <sandesh.hegde@camcom.ai>
2020-11-29 16:32:12 +0530

    adding test and inference script

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 import configparser
 import glob
 import os
-import cv2
 
+import pandas as pd
 import torch
 import torch.nn.functional as F
-
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 import global_config
 from CGRU import CGRU
+from CustomDataLoader import CustomDataset
 
 cfg = configparser.ConfigParser()
 cfg.read(global_config.model_config)
@@ -22,6 +24,8 @@ image_fn = "".join(image_fn)
 letters = sorted(list(set(list(image_fn))))
 print(len(letters))
 print(letters)
+# print(image_fn)
+
 
 vocabulary = ["-"] + letters
 print('voc', len(vocabulary))
@@ -34,6 +38,7 @@ print(char2idx)
 
 number_chars = len(char2idx)
 print(number_chars)
+
 
 def decode_predictions(text_batch_logits):
     global idx2char
@@ -50,30 +55,31 @@ def decode_predictions(text_batch_logits):
     return text_batch_tokens_new
 
 
+val_dataset = CustomDataset(val_dir)
+
+val_dataloader = DataLoader(val_dataset, batch_size=4,
+                            num_workers=4, shuffle=False)
+
+results_test = pd.DataFrame(columns=['actual', 'prediction'])
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
 cgru = CGRU(number_chars)
 cgru.load_state_dict(torch.load(MODEL_PATH, map_location=device))
-ipath = 'dataset/test/B624HM70.png'
+
+print('model loaded..............................')
+
 with torch.no_grad():
-    img = cv2.imread(ipath)
+    for image_batch, text_batch in tqdm(val_dataloader, leave=True):
+        text_batch_logits = cgru(image_batch.to(device))  # [T, batch_size, num_classes==num_features]
+        text_batch_pred = decode_predictions(text_batch_logits.cpu())
+        # print(text_batch, text_batch_pred)
+        df = pd.DataFrame(columns=['actual', 'prediction'])
+        df['actual'] = text_batch
+        df['prediction'] = text_batch_pred
+        results_test = pd.concat([results_test, df])
+results_test = results_test.reset_index(drop=True)
 
-    img = cv2.resize(img, (200, 50))
-
-    imgt = torch.tensor(img)
-    imgt = imgt.permute(2, 0, 1)
-    imgt = torch.unsqueeze(imgt, 0)
-    print(imgt.shape)
-
-    text_batch_logits = cgru(imgt.to(device).float())  # [T, batch_size, num_classes==num_features]
-    text_batch_pred = decode_predictions(text_batch_logits.cpu())
-
-
-for t in text_batch_pred:
-    print(t.replace('-', ''))
-
-
-
-
+print(results_test.shape)
+print(results_test.head())
